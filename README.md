a6ac1fcc4fc13b0eb26287c33cb31f2a7c8b44d0
Gavrie Philipson <gavrie.philipson@ultimagen.com>
2024-04-11 09:09:25 +0000

    pyo3 wip

---
+++ README.md
@@ -0,0 +1,10 @@
+pip install uv
+uv venv
+source .venv/bin/activate
+
+uv pip compile
+uv pip compile requirements.in -o requirements.txt
+uv pip install -r requirements.txt
+
+export LIBTORCH_USE_PYTORCH=1
+cargo build
--- build.rs
+++ build.rs
@@ -8,9 +8,14 @@ fn main() {
                     lib_path.to_string_lossy()
                 );
             }
-            println!("cargo:rustc-link-arg=-Wl,--no-as-needed"); // not for macos
-            println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
-            println!("cargo:rustc-link-arg=-ltorch");
+            // println!("cargo:rustc-link-arg=-Wl,--no-as-needed"); // not for macos
+            // println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
+            println!("cargo:rustc-link-lib=torch");
+            println!("cargo:rustc-link-lib=python3.9");
+            // /usr/lib/aarch64-linux-gnu/
+            // = note: use the `cargo:rustc-link-lib` directive to specify the native libraries to link with Cargo 
+            // (see https://doc.rust-lang.org/cargo/reference/build-scripts.html#cargorustc-link-libkindname)
+
         }
         _ => {}
     }
