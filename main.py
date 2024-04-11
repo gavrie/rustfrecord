7243396a672fdac9d7c16b91e17ddfb487d181a7
Gavrie Philipson <gavrie.philipson@ultimagen.com>
2024-04-11 21:54:32 +0300

    Improve Python client

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor
 
 from rustfrecord import Reader
 
@@ -7,4 +8,20 @@ r = Reader(filename, compressed=True)
 
 
 for (i, features) in enumerate(r):
-    print(i, features['label'])
+    # print(i, features.keys())
+    # ['variant_type',
+    #  'image/encoded',
+    #  'image/shape',
+    #  'variant/encoded',
+    #  'label',
+    #  'alt_allele_indices/encoded',
+    #  'locus',
+    #  'sequencing_type']
+
+    label: Tensor = features['label']
+    shape = torch.Size(tuple(features['image/shape']))
+    image: Tensor = features['image/encoded'][0].reshape(shape)
+
+    print(i, label, image.shape)
+
+    # break
